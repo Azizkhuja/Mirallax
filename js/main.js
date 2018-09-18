e54824a0bb71e36c381702d74bbb3d6930809384
Azeez <aziz.khadjayev070@gmail.com>
2018-09-19 00:39:17 +0200

    frist

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    //Year
+    $('#year').text(new Date().getFullYear());
+    //ScrollSpy
+    $('body').scrollspy({target: '#main-nav'});
+    //Smooth Scroll
+    $("#main-nav a").on('click', function (event) {
+        if (this.hash !== "") {
+          event.preventDefault();
+  
+          const hash = this.hash;
+  
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          }, 800, function () {
+  
+            window.location.hash = hash;
+          });
+        }
+      });  
+});
